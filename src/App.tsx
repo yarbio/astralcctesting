68f7374658a44bdae7ed48ce369dbde52a264a2b
raycast6000 <raycast6000@gmail.com>
2024-10-09 23:56:21 -0300

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+      <div className='flex justify-between m-10 backdrop-blur-xl'>
+        <div className='flex items-center'><h1 className='select-none text-center'>ASTRALIS</h1></div>
+        <div className='flex space-x-10 items-center'>
+          <a>HOME</a>
+          <a>FEATURES</a>
+          <a>PRICING</a>
+        </div>
+        <div><button className='bg-white text-black p-2 rounded-3xl'>JOIN US</button></div>
+      </div>
+      <h1 className='text-red-500'>i'm gay</h1>
+   </> 
+  )
+}
+
+export default App
