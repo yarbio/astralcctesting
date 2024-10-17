b46dfde98eab16ece2fcc456e3c28bbc7ad37ec9
raycast6000 <raycast6000@gmail.com>
2024-10-17 16:32:14 -0300

    backup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import './App.css'
 
+function FeatureCard(props: { title: string, description: string }) {
+  return (
+    <div className='border-4 m-2 rounded-xl'>
+      <div className='bg-transparent'>
+        <h3 className='bg-transparent text-center'>{props.title}</h3>
+      </div>
+      <p className='text-center bg-transparent'>{props.description}</p>
+    </div>
+  )
+}
+
 function App() {
   return (
-   <>
+    <>
       <div className='flex justify-between m-10 backdrop-blur-xl'>
         <div className='flex items-center'><h1 className='select-none text-center'>ASTRALIS</h1></div>
         <div className='flex space-x-10 items-center'>
@@ -12,8 +23,22 @@ function App() {
         </div>
         <div><button className='bg-white text-black p-2 rounded-3xl'>JOIN US</button></div>
       </div>
-      <h1 className='text-red-500'>i'm gay</h1>
-   </> 
+
+      <div className='flex w-screen justify-center'>
+        <div className='max-w-5xl'>
+          <div>
+            <h1 className='text-[70px] font-sg text-center'>Astralis</h1>
+            <h3 className='text-center'>If you can make her laugh and giggle you can make them cheeks clap and jiggle.</h3>
+          </div>
+
+          <div className='flex'>
+            <FeatureCard title='Seamless Execution' description='lorem ipsum dolor sit amet r bec an awdp' />
+            <FeatureCard title='Zero Downtime' description='lorem ipsum dolor sit amet r bec an awdp' />
+            <FeatureCard title='24/7 Support' description='lorem ipsum dolor sit amet r bec an awdp' />
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./src/**/*.{html,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      'sg': ["Space Grotesk", 'sans-serif']
+    }
   },
   plugins: [],
 }
